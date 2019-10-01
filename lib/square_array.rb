4f90c4fa3c44089dd98b1bc53ada5d6012dc1953
rtfp0102 <rtfp0102@gmail.com>
2019-10-01 01:06:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_numbers = []
   
   while counter < array.length do
-    new_numbers << array[counter] ** 2 
+    new_numbers << array[counter]**2 
     counter += 1 
   end
   
